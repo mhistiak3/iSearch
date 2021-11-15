0495077199b7aa1afca92e415dba7a79d17f7051
Istiak <mhistiak2@gmail.com>
2021-11-15 19:05:58 +0600

    Fixed some Issue

--- src/components/Links.js
+++ src/components/Links.js
@@ -8,16 +8,22 @@ const Links = () => {
   ];
   return (
     <div className="flex sm:justify-around justify-between items-center mt-4 w-full">
-      {links.map(({ url, text },index) => (
+      {links.map(({ url, text }, index) => (
         <NavLink
-        key={index}
+          key={index}
           to={url}
           activeClassName="text-blue-700 border-b-2 dark:text-blue-300 border-blue-700 pb-2"
         >
           {text}
         </NavLink>
       ))}
-      <a href="https://www.youtube.com/results?search_query=php" target='_blank'>🎥 Videos</a>
+      <a
+        href="https://www.youtube.com/results?search_query=php"
+        rel="noreferrer"
+        target="_blank"
+      >
+        🎥 Videos
+      </a>
     </div>
   );
 };
